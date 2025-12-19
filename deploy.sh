eeced106307ba87cf06c4bb6fda6a9cda5410d4e
Rajesh-210 <rajeshchilukuri210@gmail.com>
2025-12-19 10:18:19 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 set -e
 
-APP_DIR="$HOME/success-project/success-project"
-
-cd ${APP_DIR}
-
-echo "🔄 Restarting application containers..."
+cd "$WORKSPACE/success-project/success-project"
 
 docker compose down
 docker compose up -d
 
 echo "======================================="
-echo "✅ APPLICATION RESTARTED SUCCESSFULLY"
-echo "Frontend: http://<SERVER_PUBLIC_IP>"
-echo "Backend : http://<SERVER_PUBLIC_IP>:8080"
+echo "✅ APPLICATION REDEPLOYED"
 echo "======================================="
